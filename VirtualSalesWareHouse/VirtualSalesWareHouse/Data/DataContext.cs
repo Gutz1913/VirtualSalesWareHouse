ffd225dfe8b35d597e51047981929ad49b05080a
Gutz1913 <andresfgutierrezv@gmail.com>
2026-03-26 17:42:37 -0500

    Workin on my new WebSite VirtualSalesWareHouse, I created the database context with the first table, countries. I also created and applied the migrations and the complete CRUD operations for the countries table.

---
+++ VirtualSalesWareHouse/VirtualSalesWareHouse/Data/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VirtualSalesWareHouse.Data.Entities;
+
+namespace VirtualSalesWareHouse.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {        
+    }
+
+    public DbSet<Country> Countries { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Country>(entity =>
+        {
+            entity.HasIndex(c => c.Name).IsUnique();
+        });
+    }
+}
