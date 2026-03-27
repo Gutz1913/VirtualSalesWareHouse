d0bd71ff44b20ea87e0284de1ce5e6fe0de6f5af
Gutz1913 <andresfgutierrezv@gmail.com>
2026-03-26 20:36:04 -0500

    Updating views and customizing buttons

--- VirtualSalesWareHouse/VirtualSalesWareHouse/Controllers/CountriesController.cs
+++ VirtualSalesWareHouse/VirtualSalesWareHouse/Controllers/CountriesController.cs
@@ -14,13 +14,13 @@ public class CountriesController : Controller
         _context = context;
     }
 
-    // GET: Countries
+    [HttpGet]
     public async Task<IActionResult> Index()
     {
         return View(await _context.Countries.ToListAsync());
     }
 
-    // GET: Countries/Details/5
+    [HttpGet]
     public async Task<IActionResult> Details(int? id)
     {
         if (id == null)
@@ -38,18 +38,16 @@ public class CountriesController : Controller
         return View(country);
     }
 
-    // GET: Countries/Create
+    [HttpGet]
     public IActionResult Create()
     {
         return View();
     }
 
-    // POST: Countries/Create
-    // To protect from overposting attacks, enable the specific properties you want to bind to.
-    // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+
     [HttpPost]
     [ValidateAntiForgeryToken]
-    public async Task<IActionResult> Create([Bind("Id,Name")] Country country)
+    public async Task<IActionResult> Create(Country country)
     {
         if (ModelState.IsValid)
         {
