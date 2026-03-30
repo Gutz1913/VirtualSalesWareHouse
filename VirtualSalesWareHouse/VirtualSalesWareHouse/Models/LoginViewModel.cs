fed6d405667b6df82bd82c792f6f81b259b1f456
Gutz1913 <andresfgutierrezv@gmail.com>
2026-03-30 18:27:40 -0500

    updates

--- VirtualSalesWareHouse/VirtualSalesWareHouse/Models/LoginViewModel.cs
+++ VirtualSalesWareHouse/VirtualSalesWareHouse/Models/LoginViewModel.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Components.Forms;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace VirtualSalesWareHouse.Models;
 
@@ -17,5 +16,5 @@ public class LoginViewModel
     public string Password { get; set; }
 
     [Display(Name = "Recordarme en este navegador")]
-    public string RememberMe { get; set; }
+    public bool RememberMe { get; set; }
 }
