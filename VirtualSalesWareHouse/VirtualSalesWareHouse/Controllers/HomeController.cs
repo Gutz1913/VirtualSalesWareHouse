ff292d1e2429492d34d551438dc5809aa8774709
Gutz1913 <andresfgutierrezv@gmail.com>
2026-04-03 11:45:02 -0500

    Home view modified.

--- VirtualSalesWareHouse/VirtualSalesWareHouse/Controllers/HomeController.cs
+++ VirtualSalesWareHouse/VirtualSalesWareHouse/Controllers/HomeController.cs
@@ -1,14 +1,56 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
+using VirtualSalesWareHouse.Data;
+using VirtualSalesWareHouse.Data.Entities;
 using VirtualSalesWareHouse.Models;
 
 namespace VirtualSalesWareHouse.Controllers;
 
 public class HomeController : Controller
 {
-    public IActionResult Index()
+    private readonly ILogger<HomeController> _logger;
+    private readonly DataContext _context;
+
+    public HomeController(ILogger<HomeController> logger, DataContext context)
     {
-        return View();
+        _logger = logger;
+        _context = context;
+    }
+    public async Task<IActionResult> Index()
+    {
+        List<Product> products = await _context.Products
+            .Include(p => p.ProductImages)
+            .Include(p => p.ProductCategories)
+            .OrderBy(p => p.Description)
+            .ToListAsync();
+
+        List<ProductsHomeViewModel> productsHome = new() { new ProductsHomeViewModel() };
+        int i = 1;
+        foreach (Product product in products)
+        {
+            if (i == 1)
+            {
+                productsHome.LastOrDefault().Product1 = product;
+            }
+            if (i == 2)
+            {
+                productsHome.LastOrDefault().Product2 = product;
+            }
+            if (i == 3)
+            {
+                productsHome.LastOrDefault().Product3 = product;
+            }
+            if (i == 4)
+            {
+                productsHome.LastOrDefault().Product4 = product;
+                productsHome.Add(new ProductsHomeViewModel());
+                i = 0;
+            }
+            i++;
+        }
+
+        return View(productsHome);
     }
 
     public IActionResult Privacy()
