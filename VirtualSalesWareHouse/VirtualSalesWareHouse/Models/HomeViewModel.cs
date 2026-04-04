a61f3fe10653979ca73746bcaa67bb5520c9a1e1
Gutz1913 <andresfgutierrezv@gmail.com>
2026-04-04 17:37:18 -0500

    Order, filter, pagination in home and add Privacy Polices

--- VirtualSalesWareHouse/VirtualSalesWareHouse/Models/HomeViewModel.cs
+++ VirtualSalesWareHouse/VirtualSalesWareHouse/Models/HomeViewModel.cs
@@ -1,9 +1,11 @@
-﻿using VirtualSalesWareHouse.Data.Entities;
+﻿using VirtualSalesWareHouse.Common;
+using VirtualSalesWareHouse.Data.Entities;
 
 namespace VirtualSalesWareHouse.Models;
 
 public class HomeViewModel
 {
-    public ICollection<Product> Products { get; set; }
+    public PaginatedList<Product> Products { get; set; }
+    public ICollection<Category> Categories { get; set; }
     public float Quantity { get; set; }
 }
